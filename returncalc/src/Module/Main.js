7f54e42b3e60a57b0f7e03ed8289d68c99fadb6d
Hemanth Bhupatiraju <hbhupatiraju@gmail.com>
2019-04-25 17:29:56 -0400

    Third Commit

--- returncalc/src/Module/Main.js
+++ returncalc/src/Module/Main.js
@@ -7,83 +7,109 @@ class Main extends React.Component {
     super();
    
     this.state = {
+      //savings factor
       savings: 0,
+      //number of tickets in a  month
       NumTickets: 0,
+      //cost of one support person
       Cost: 0,
+      //Company's investment
       Investment: 0,
+      //Hours that one support person works for
       Hours: 0,
+      //time it takes to complete one ticket
       Time: 0
     }
   };
-  
+  //Method to output the ROI and the number of workers needed
   computeROI(){
+    //sets a variable equal to the total time it will take to complete all tickets in a month
     const totalTicketTime = this.state.NumTickets*this.state.Time;
+    //sets a variable equal to the total time it will take to complete all tickets in a month dividede by the number of hours one support person works
     const numWorkers = totalTicketTime/this.state.Hours;
+    //sets a variable equal to the cost of one support person times the persons saving factor
     const amountSaved = this.state.Cost*this.state.savings;
+    //sets the returnInvestment variable equal to the amount saved subtracted by the companys investment divided by the companys investment
     const returnInvestment = (amountSaved-this.state.Investment)/this.state.Investment;
+    //returns an alert that states the ROI and the number of workers needed
     alert('Your ROI is ' + (returnInvestment*100) +'% The number of workers you need is ' + numWorkers);
 
   }
   render() {
     return (
       <div>
-         <div className = "time">
-               <form>
-        <label>
-       Time per Ticket(in Hours):
-        </label>
-        <input defaultValue = "0" type="number" onChange={e => this.setState({Time: e.target.value})}/>
-       
-        </form>
-        </div>
-        <div className = "Name">
+       {/* Form for time it takes to complete one ticket*/}
         <form>
-<         label>
-               Number of Tickets Per Month:
-              </label>
-              <input defaultValue = "0" type="number" onChange={e => this.setState({NumTickets: e.target.value})}/>
-
-              </form>
-              </div>
-              
-               <div className = "hours">
-               <form>
-               <label>
-               Working Hours Per Person Per Month:
-               </label>
-               <input defaultValue = "0" type="number" onChange={e => this.setState({Hours: e.target.value})}/>
-              
-               </form>
-               </div>
-    <div className = "cost">
-    <form>
-      <label>
-      Annual Cost Per One Support Person:
-    </label>
-     <input defaultValue = "0" type="number" onChange={e => this.setState({Cost: e.target.value})}/>
-      </form>
-               </div>
-      <div className = "saving">
+        <h4 className = "timelabel">
+       Time per Ticket(in Hours):
+        </h4>
+        <input defaultValue = "0" type="number" onChange={e => this.setState({Time: e.target.value})} className = "time"/>
+        </form>
       
-        <label>
-          Persons Saving Factor:
-        </label>
-        <input  defaultValue = "0" type="number" onChange={e => this.setState({savings: e.target.value})}/>
-       
-        </div>
-       
+        {/* Form for the number of tickets there are per month*/}
+        <form>
+               <h4 className = "ticketslabel">
+               Number of Tickets Per Month:
+              </h4>
+              <input defaultValue = "0" type="number" onChange={e => this.setState({NumTickets: e.target.value})} className = "tickets"/>
+           
+             </form>
               
-              <div className ="invest">
+              
+             {/* Form for the number of hours that one support person works for a month*/}
+              <form>
+               <h4 className = "hourslabel">
+               Working Hours Per Person Per Month:
+               </h4>
+               <input defaultValue = "0" type="number" onChange={e => this.setState({Hours: e.target.value})} className = "hours"/>
+              </form>
+               
+              {/* Form for the annual cost of support person*/}
                <form>
-               <label>
+      <h4 className = "costlabel">
+      Annual Cost Per One Support Person($):
+    </h4>
+     <input defaultValue = "0" type="number" onChange={e => this.setState({Cost: e.target.value})} className = "cost"/>
+      </form>
+        {/* Form for persons saving factor*/}      
+        
+        <form>
+        <h4 className = "savinglabel">
+          Persons Saving Factor:
+        </h4>
+        <input  defaultValue = "0" type="number" onChange={e => this.setState({savings: e.target.value})} className = "saving"/>
+    
+        </form>
+        
+             {/* Form for the company investment*/}  
+             <form>
+              
+               <h4 className = "investlabel">
                  Company Investment($):
-               </label>
-               <input  defaultValue = "0" type="number" onChange={e => this.setState({Investment: e.target.value})} />
+               </h4>
+               <input  defaultValue = "0" type="number" onChange={e => this.setState({Investment: e.target.value})} className = "invest"/>
                </form>
-               </div>
+               {/* Div for the submit button*/}
                <div className = "submit">
                <a onClick = {(e) => {e.preventDefault(); this.computeROI();}}>Calculate</a>
                </div>
+                {/* Help Box*/}
+              <div className = "box">
+              <p className = "ROIHELP1">
+              <a href= "https://www.investopedia.com/terms/r/returnoninvestment.asp"> What is ROI?</a>
+              </p>
+              <p className = "ROIHELP2">
+              <a href= "https://www.investopedia.com/articles/basics/10/guide-to-calculating-roi.asp"> How is ROI calculated? </a>
+              </p>
+              
+              </div>
+                <h3 className = "help">
+                  Help:
+                </h3>
+
+                
+             
+             
                </div>
     );
   }
